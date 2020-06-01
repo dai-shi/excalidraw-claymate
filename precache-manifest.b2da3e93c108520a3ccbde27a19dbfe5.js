21dee37a05aeaf30d97876495446425f6f8a36d6
dai-shi <dai-shi@users.noreply.github.com>
2020-06-01 15:24:40 +0000

    deploy: a5382f2ba94b2618b9e28c8030d4a62607bd638c

---
+++ precache-manifest.b2da3e93c108520a3ccbde27a19dbfe5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64aa268b1d908e739e32aca4974bf7a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a423b78481c461f8982",
+    "url": "/static/css/main.8ed0b6fe.chunk.css"
+  },
+  {
+    "revision": "27d6686dbab15ef95f5f",
+    "url": "/static/js/2.bfbefef6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bfbefef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43012260376f254426d",
+    "url": "/static/js/3.42570b3c.chunk.js"
+  },
+  {
+    "revision": "7376fc9b76294660e8b3",
+    "url": "/static/js/4.63c06711.chunk.js"
+  },
+  {
+    "revision": "b441df5069d9fbdc764c",
+    "url": "/static/js/5.41b16397.chunk.js"
+  },
+  {
+    "revision": "e68b12a0f2b6028710d4",
+    "url": "/static/js/6.507e69b6.chunk.js"
+  },
+  {
+    "revision": "7aff7b8171e2f4f9532a",
+    "url": "/static/js/7.fedeee78.chunk.js"
+  },
+  {
+    "revision": "71c07b7eacf11474815b",
+    "url": "/static/js/8.62ef7cd9.chunk.js"
+  },
+  {
+    "revision": "9a423b78481c461f8982",
+    "url": "/static/js/main.6a61986a.chunk.js"
+  },
+  {
+    "revision": "f1c60822e057e2fe0f79",
+    "url": "/static/js/runtime-main.63226dcd.js"
+  }
+]);
