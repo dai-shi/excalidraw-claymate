61516d7ec010994241146a6df0f3c16d70d8fca9
daishi <daishi@axlight.com>
2020-06-01 23:50:47 +0900

    add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a button', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = getByText(/add/i);
+  expect(buttonElement).toBeInTheDocument();
 });
