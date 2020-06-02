f68fd1e35bfd3597ec8838e64cae9343264ebf08
daishi <daishi@axlight.com>
2020-06-03 01:06:59 +0900

    make test work

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import "jest-canvas-mock";
+const element = document.createElement("div");
+element.id = "root";
+document.body.appendChild(element);
