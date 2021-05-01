5818852d3094a779cee407e57e093030bc840999
grant-cpl <grant@cognitiveperformancelabs.com>
2021-05-02 10:56:55 +1200

    Use node crypto to implement window.crypto.getRandomValues for nanoid

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,12 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 import "jest-canvas-mock";
+import crypto from "crypto";
+window.crypto = {
+  getRandomValues: function (buffer: T) {
+    return crypto.randomFillSync(buffer);
+  },
+};
 const element = document.createElement("div");
 element.id = "root";
 document.body.appendChild(element);
