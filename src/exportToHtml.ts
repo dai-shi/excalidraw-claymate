2d8e5a989e9a5f0ad71d78f0cca88942beb8c60d
grant-davidson <grant@davidsonclan.co.nz>
2021-05-12 20:53:12 +1200

    HTML slide navigation buttons and current slide indicator

--- src/exportToHtml.ts
+++ src/exportToHtml.ts
@@ -4,26 +4,46 @@ import { exportToSvg } from "@excalidraw/excalidraw";
 import { Scene } from "./types";
 
 export const exportToHtml = async (scenes: Scene[]) => {
-  let html = `
-    <html>
+  let html = `<!DOCTYPE html>
+    <html lang="en">
       <style>
         svg { width: 100%; height: 100%; }
+        #container { display: flex; flex-direction: column; height: 100%; }
+        #navigation { display: flex; justify-content: center; align-items: center; padding: 5px;}
+        .navbutton { padding: 3px; margin: 0px 5px; }
+        #slides { height: calc(100vh - 50px); }                
       </style>
       <script>
         let index = 0;
+        let totalScenes = ${scenes.length}
+        function updateTitle() {
+          document.getElementById('title').innerText = index+1 + " of " + totalScenes;
+        }
+        function moveLeft() {
+          if (index > 0) {
+            document.getElementById('scene' + index).style.display = 'none';
+            index -= 1;
+            document.getElementById('scene' + index).style.display = 'block';
+            updateTitle();
+          }
+        }
+        function moveRight() {
+          if (index < totalScenes - 1) {
+            document.getElementById('scene' + index).style.display = 'none';
+            index += 1;
+            document.getElementById('scene' + index).style.display = 'block';
+            updateTitle();
+          }
+        }
         document.addEventListener('DOMContentLoaded', () => {
           document.getElementById('scene' + index).style.display = 'block';
         });
         document.addEventListener('keydown', (event) => {
-          if (event.key === 'ArrowRight' && index < ${scenes.length - 1}) {
-            document.getElementById('scene' + index).style.display = 'none';
-            index += 1;
-            document.getElementById('scene' + index).style.display = 'block';
+          if (event.key === 'ArrowRight') {
+            moveRight();
           }
-          if (event.key === 'ArrowLeft' && index > 0) {
-            document.getElementById('scene' + index).style.display = 'none';
-            index -= 1;
-            document.getElementById('scene' + index).style.display = 'block';
+          if (event.key === 'ArrowLeft') {
+            moveLeft();
           }
           if (event.key.toLowerCase() === "f") {
             if (document.fullscreenElement === document.body) {
@@ -35,6 +55,8 @@ export const exportToHtml = async (scenes: Scene[]) => {
         });
       </script>
       <body>
+      <div id="container">
+      <div id="slides">
   `;
   scenes.forEach((scene, index) => {
     const svg: SVGSVGElement = exportToSvg(scene.drawing);
@@ -42,7 +64,13 @@ export const exportToHtml = async (scenes: Scene[]) => {
     svg.style.display = "none";
     html += svg.outerHTML;
   });
-  html += "</body></html>";
+  html += `</div>
+            <div id="navigation">
+              <button class="navbutton" type="button" onClick="moveLeft()">&#9664;</button>
+              <div id="title">1 of ${scenes.length}</div>
+              <button class="navbutton" type="button" onClick="moveRight()">&#9654;</button>          
+            </div>
+        </div></body></html>`;
   await fileSave(new Blob([html], { type: "text/html" }), {
     fileName: "excalidraw-claymate.html",
   });
