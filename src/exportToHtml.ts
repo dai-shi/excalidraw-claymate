a883f539f4a0e26e413ed2e85edfdd9b28522c6f
grant-davidson <grant@davidsonclan.co.nz>
2021-05-12 22:43:59 +1200

    Button inherit font-size from body

--- src/exportToHtml.ts
+++ src/exportToHtml.ts
@@ -8,10 +8,10 @@ export const exportToHtml = async (scenes: Scene[]) => {
     <html lang="en">
       <style>
         svg { width: 100%; height: 100%; }
-        body { margin: 0px; font-size: 20px; }
+        body { margin: 0px; font-size: 24px; }
         #container { display: flex; flex-direction: column; height: 100%; }
         #navigation { display: flex; justify-content: center; align-items: center; padding: 5px;}
-        .navbutton { padding: 3px; margin: 0px 10px; width: 30px; height: 30px; }
+        .navbutton { padding: 3px; margin: 0px 10px; font-size: inherit; }
         #slides { height: calc(100vh - 50px); }                
       </style>
       <script>
