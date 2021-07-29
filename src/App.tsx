14ef290e0b2802aaa03683bc407ed1f4fc5eae6f
grant-davidson <grant@davidsonclan.co.nz>
2021-07-29 20:47:54 +1200

    Support Excalidraw libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useMemo, useRef } from "react";
 import Excalidraw from "@excalidraw/excalidraw";
 import type {
   ExcalidrawAPIRefValue,
@@ -8,6 +8,7 @@ import "./App.css";
 import Claymate from "./Claymate";
 import { Drawing } from "./types";
 import { useScenes } from "./useScenes";
+import { useLibrary } from "./useLibrary";
 
 const App: React.FC = () => {
   const excalidrawRef = useRef<ExcalidrawAPIRefValue>(null);
@@ -16,16 +17,26 @@ const App: React.FC = () => {
       drawing
     );
   };
+
   const {
     moveToScene,
     addScene,
     onChange,
     drawingVersion,
     currentIndex,
-    initialData,
+    initialData: initialSceneData,
     scenes,
     updateScenes,
   } = useScenes();
+
+  const { onLibraryChange, libraryItems } = useLibrary();
+
+  const initialData = useMemo(() => {
+    if (libraryItems) {
+      return { ...initialSceneData, libraryItems };
+    }
+    return initialSceneData;
+  }, [initialSceneData, libraryItems]);
 
   return (
     <div className="ClaymateApp">
@@ -34,6 +45,7 @@ const App: React.FC = () => {
         key={drawingVersion}
         initialData={initialData}
         onChange={onChange}
+        onLibraryChange={onLibraryChange}
       />
       <Claymate
         scenes={scenes}
---
+++ src/useLibrary.ts
@@ -0,0 +1,33 @@
+import { LibraryItems } from "@excalidraw/excalidraw/types/types";
+import { useCallback, useMemo, useRef } from "react";
+
+const LIBRARY_STORAGE_KEY = "claymate-libraries";
+
+export const loadLibraries = (): LibraryItems | undefined => {
+  try {
+    const storage = localStorage.getItem(LIBRARY_STORAGE_KEY);
+    const items = storage ? JSON.parse(storage) : undefined;
+    if (items) {
+      return items as LibraryItems;
+    }
+  } catch (e) {
+    console.error("Unable to load libraries", e);
+  }
+  return undefined;
+};
+
+export const saveLibraries = (items: LibraryItems) => {
+  const result = JSON.stringify(items);
+  localStorage.setItem(LIBRARY_STORAGE_KEY, result);
+};
+
+export const useLibrary = () => {
+  const initialLibraryItems = useMemo(() => loadLibraries(), []);
+  const libraryRef = useRef<LibraryItems | undefined>(initialLibraryItems);
+  const onLibraryChange = useCallback((items: LibraryItems) => {
+    saveLibraries(items);
+    libraryRef.current = items;
+  }, []);
+
+  return { onLibraryChange, libraryItems: libraryRef.current };
+};
