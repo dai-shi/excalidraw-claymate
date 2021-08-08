d2afce18ac5eba6b24782b720a68e4ff19e6d05b
Daishi Kato <dai-shi@users.noreply.github.com>
2021-08-08 21:46:42 +0900

    feat: animation controller in exported html (#45)

--- src/exportToHtml.ts
+++ src/exportToHtml.ts
@@ -4,7 +4,7 @@ import type {
   ExcalidrawElement,
   NonDeletedExcalidrawElement,
 } from "@excalidraw/excalidraw/types/element/types";
-import { animateSvg } from "excalidraw-animate/dist/library";
+import { animateSvg, getBeginTimeList } from "excalidraw-animate/dist/library";
 import { AnimateOptions } from "./AnimateConfig";
 
 import { Scene } from "./types";
@@ -22,6 +22,50 @@ type Options = {
 };
 
 export const exportToHtml = async (scenes: Scene[], options: Options) => {
+  const animateFunctions = options.animate
+    ? `
+        function togglePausedAnimations() {
+          const svg = document.getElementById('scene' + index);
+          if (svg.animationsPaused()) {
+            for (const svg of document.getElementsByTagName('svg')) {
+              svg.unpauseAnimations();
+            }
+          } else {
+            for (const svg of document.getElementsByTagName('svg')) {
+              svg.pauseAnimations();
+            }
+          }
+        }
+        const beginTimeLists = [];
+        let animateTimer;
+        function stepForwardAnimations() {
+          const svg = document.getElementById('scene' + index);
+          const beginTimeList = beginTimeLists[index];
+          const currentTime = svg.getCurrentTime() * 1000;
+          let nextTime = beginTimeList.find((t) => t > currentTime + 50);
+          console.log(currentTime, beginTimeList, nextTime);
+          if (nextTime) {
+            nextTime -= 1;
+          } else {
+            nextTime = currentTime + 500;
+          }
+          clearTimeout(animateTimer);
+          svg.unpauseAnimations();
+          animateTimer = setTimeout(() => {
+            svg.pauseAnimations();
+            svg.setCurrentTime(nextTime / 1000);
+          }, nextTime - currentTime);
+        }
+        function resetAnimations() {
+          const svg = document.getElementById('scene' + index);
+          svg.setCurrentTime(0);
+        }
+`
+    : `
+        function togglePausedAnimations() {}
+        function stepForwardAnimations() {}
+        function resetAnimations() {}
+`;
   let html = `<!DOCTYPE html>
     <html lang="en">
       <style>
@@ -29,9 +73,10 @@ export const exportToHtml = async (scenes: Scene[], options: Options) => {
         body { margin: 0px; font-size: 24px; }
         button { background: transparent; border: none; cursor: pointer; padding: 3px; margin: 0px 10px; font-size: inherit;}
         #container { display: flex; flex-direction: column; height: 100%; }
-        #navigation { display: flex; justify-content: center; align-items: center; padding: 5px; border-top: 1px solid lightgray; background: white; }        
-        #rightbuttons { position: absolute; right: 10px; display: flex;  }
-        #slides { height: calc(100vh - 50px); }                
+        #navigation { display: flex; justify-content: center; align-items: center; padding: 5px; border-top: 1px solid lightgray; background: white; }
+        #leftbuttons { position: absolute; left: 10px; display: flex; }
+        #rightbuttons { position: absolute; right: 10px; display: flex; }
+        #slides { height: calc(100vh - 50px); }
       </style>
       <script>
         let index = 0;
@@ -68,6 +113,7 @@ export const exportToHtml = async (scenes: Scene[], options: Options) => {
             document.body.requestFullscreen();
           }
         }
+        ${animateFunctions}
         document.addEventListener('DOMContentLoaded', () => {
           document.getElementById('scene' + index).style.display = 'block';
           document.getElementById('scene' + index).setCurrentTime(0);
@@ -80,7 +126,16 @@ export const exportToHtml = async (scenes: Scene[], options: Options) => {
             moveLeft();
           }
           if (event.key.toLowerCase() === 'f') {
-            toggleMaximise();            
+            toggleMaximise();
+          }
+          if (event.key.toLowerCase() === 'p') {
+            togglePausedAnimations();
+          }
+          if (event.key.toLowerCase() === 's') {
+            stepForwardAnimations();
+          }
+          if (event.key.toLowerCase() === 'r') {
+            resetAnimations();
           }
         });
       </script>
@@ -101,18 +156,39 @@ export const exportToHtml = async (scenes: Scene[], options: Options) => {
     svg.id = `scene${index}`;
     svg.style.display = "none";
     html += svg.outerHTML;
+    if (options.animate) {
+      const beginTimeList = getBeginTimeList(svg);
+      beginTimeList.sort((a, b) => a - b);
+      html += `
+        <script>
+          beginTimeLists.push(${JSON.stringify(beginTimeList)});
+        </script>
+      `;
+    }
   }
-  html += `</div>
-            <div id="navigation">
-              <button class="navbutton" type="button" onClick="moveLeft()" title="Previous slide">&#9664;</button>
-              <div id="title">1 of ${scenes.length}</div>
-              <button class="navbutton" type="button" onClick="moveRight()" title="Next slide">&#9654;</button>          
-              <div id="rightbuttons">
-                <button type="button" onClick="toggleMaximise()" title="Toggle full-screen">&#x26F6;</button>
-                <button type="button" onClick="closeNavigation()" title="Close this panel">&#x2716;</button>
-              <div>
-            </div>
-        </div></body></html>`;
+  const animateButtons = options.animate
+    ? `
+    <div id="leftbuttons">
+     <button type="button" onclick="togglePausedAnimations()" title="Play or pause animations">P</button>
+     <button type="button" onclick="stepForwardAnimations()" title="Step forward animations">S</button>
+     <button type="button" onclick="resetAnimations()" title="Reset animations">R</button>
+    </div>
+`
+    : "";
+  html += `
+    </div>
+    <div id="navigation">
+      ${animateButtons}
+      <button class="navbutton" type="button" onclick="moveLeft()" title="Previous slide">&#9664;</button>
+      <div id="title">1 of ${scenes.length}</div>
+      <button class="navbutton" type="button" onclick="moveRight()" title="Next slide">&#9654;</button>
+      <div id="rightbuttons">
+        <button type="button" onclick="toggleMaximise()" title="Toggle full-screen">&#x26F6;</button>
+        <button type="button" onclick="closeNavigation()" title="Close this panel">&#x2716;</button>
+      </div>
+    </div>
+  </div></body></html>
+`;
   await fileSave(new Blob([html], { type: "text/html" }), {
     fileName: "excalidraw-claymate.html",
   });
