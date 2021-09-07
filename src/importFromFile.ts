512eff20f2f378713d5f38a233fcc58298248003
daishi <daishi@axlight.com>
2021-09-08 01:27:43 +0900

    feat: import from svg file

--- src/importFromFile.ts
+++ src/importFromFile.ts
@@ -1,5 +1,7 @@
 import { AppState } from "@excalidraw/excalidraw/types/types";
 import { loadFromBlob } from "@excalidraw/excalidraw";
+import svgToEx from "svg-to-excalidraw";
+
 import { Drawing } from "./types";
 
 export const importFromFile = async (
@@ -13,7 +15,22 @@ export const importFromFile = async (
       appState: { ...appState, ...result.appState },
     };
   }
+  if (file.type === "image/svg+xml") {
+    const elements = await convertSvgToElements(file);
+    return { elements, appState };
+  }
   console.log("Unsupported file dropped", file);
   window.alert("Unsupported file dropped");
   return null;
+};
+
+export const convertSvgToElements = async (
+  file: File
+): Promise<Drawing["elements"]> => {
+  const text = await file.text();
+  const { hasErrors, content } = svgToEx.convert(text);
+  if (hasErrors) {
+    throw Error("Error in convertSvgToElements");
+  }
+  return content.elements;
 };
