db0970c7598a9554d5cafb4c22c0713ab6e65637
seunghyunOh <oshyun6623@gmail.com>
2022-10-05 13:39:04 +0900

    fix: occurred error by react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<StrictMode><App /></StrictMode>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
