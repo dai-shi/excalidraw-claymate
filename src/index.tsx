05fa6e340c5100559a6f9be9b3ca8ae16ce8ad41
seunghyunOh <oshyun6623@gmail.com>
2022-10-06 00:16:30 +0900

    chore: update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<StrictMode><App /></StrictMode>);
+createRoot(document.getElementById("root") as HTMLElement).render(<StrictMode><App /></StrictMode>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
