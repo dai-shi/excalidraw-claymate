ed5c0fd06e7f63fd8a582ab134a6bffa21099d20
Omar Baabouj <baabouj.dev@gmail.com>
2023-05-04 23:59:48 +0100

    feat(src/index.tsx): switch to the React 18 API

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { createRoot } from "react-dom/client";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
