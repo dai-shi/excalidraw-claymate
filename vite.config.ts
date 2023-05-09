34a21b44df51ae0eecb34066826cb70c1ba1b155
Omar Baabouj <baabouj.dev@gmail.com>
2023-05-09 11:15:14 +0100

    fix(vite.config.ts): add base option

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/excalidraw-claymate/" : "/",
   plugins: [react()],
   server: {
     port: 3000,
@@ -16,4 +17,4 @@ export default defineConfig({
     globals: true,
     setupFiles: "./src/setupTests.ts",
   },
-});
+}));
