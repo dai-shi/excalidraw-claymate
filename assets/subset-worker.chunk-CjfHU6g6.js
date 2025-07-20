20e0d6733e35244212886c9114146dc0cecfdf5a
dai-shi <dai-shi@users.noreply.github.com>
2025-07-20 03:53:00 +0000

    deploy: 1b89daa738051ed380fc82f8d59b048845ff4356

--- assets/subset-worker.chunk-CjfHU6g6.js
+++
@@ -1,1 +0,0 @@
-import{Commands as e,subsetToBinary as t}from"./subset-shared.chunk-BtssTXwt.js";import"./index-D0VDNqPQ.js";var m=import.meta.url?new URL(import.meta.url):void 0;typeof window>"u"&&typeof self<"u"&&(self.onmessage=async a=>{switch(a.data.command){case e.Subset:let s=await t(a.data.arrayBuffer,a.data.codePoints);self.postMessage(s,{transfer:[s]});break}});export{m as WorkerUrl};
